3da737787e517995d8cd034a6d5a77c618e9f24e
Kevin Wong <kevinwong_895@hotmail.com>
2017-05-13 13:27:40 -0400

    Adding components folder for application

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
 
-import App from './components/app';
-import reducers from './reducers';
+import SearchBar from './components/search_bar';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const API_KEY = 'AIzaSyBa4jOOxDpnGrC3xgz2Y6onOzOmFwYblN4';
+// Create a new component. This component should produce some HTML
+const App = function() {
+  return (
+		<div>
+    	<SearchBar/>
+    </div>
+	);
+}
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+// Take this component's generated HTML and put it on the page (in the DOM)
+
+ReactDOM.render( < App / > , document.querySelector('.container'));
