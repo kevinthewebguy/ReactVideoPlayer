86d6cb2b6e8a14f178a5d36ab81a16ca52797d1f
Kevin Wong <kevinwong_895@hotmail.com>
2017-05-14 12:06:05 -0400

    Pulling thumbnail and title from youtube api

--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@ import React, {Component }from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 
 const API_KEY = 'AIzaSyBa4jOOxDpnGrC3xgz2Y6onOzOmFwYblN4';
 
 // Create a new component. This component should produce some HTML
-class App extends Component
+class App extends Component {
 	constructor(props){
 		super(props);
 		this.state = {videos: []};
@@ -14,10 +15,12 @@ class App extends Component
 			this.setState({videos});
 		});
 	}
+
 	render(){
 		return (
 			<div>
 	    	<SearchBar/>
+				<VideoList videos={this.state.videos} />
 	    </div>
 		);
 	}
